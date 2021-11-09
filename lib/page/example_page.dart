313f1ed23318ca4e35e51a0bc65acac26ca0e4af
Funncy <gywns41@naver.com>
2021-11-09 21:28:25 +0900

    Feature : 예제 화면 구현

--- lib/page/example_page.dart
+++ lib/page/example_page.dart
@@ -1,4 +1,7 @@
+import 'package:architecture_project/core/view_state.dart';
+import 'package:architecture_project/viewmodel/example_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ExamplePage extends StatefulWidget {
   static const routeName = "/TestPage";
@@ -9,11 +12,50 @@ class ExamplePage extends StatefulWidget {
 }
 
 class _ExamplePageState extends State<ExamplePage> {
+  late ExampleViewModel _exampleViewModel;
+
+  @override
+  void initState() {
+    _exampleViewModel = Get.find<ExampleViewModel>();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("data"),
+        child: Obx(() {
+          //viewmodel의 viewstate가져오기 => !붙인 이유는 nullable을 non-nullable로 풀어주려고
+
+          //화면 상태에 따라 화면 그려주기.
+          ViewState state = _exampleViewModel.viewState!;
+
+          if (state is Initial) {
+            return TextButton(
+                onPressed: () {
+                  _exampleViewModel.getExampleList();
+                },
+                child: const Text("데이터 가져오기"));
+          }
+
+          if (state is Error) {
+            return const Text("Error!");
+          }
+
+          if (state is Loading) {
+            return const CircularProgressIndicator();
+          }
+
+          return ListView.builder(
+            itemBuilder: (_, index) {
+              return ListTile(
+                title: Text(_exampleViewModel.exampleList[index].contents),
+              );
+            },
+            itemCount: _exampleViewModel.exampleList.length,
+          );
+        }),
       ),
     );
   }
