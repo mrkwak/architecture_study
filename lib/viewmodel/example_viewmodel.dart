220b59262a64c739cd31b4b479a263d15cf73ecc
mrkwak <qudcjfrhkr@naver.com>
2021-11-16 17:24:38 +0900

    수정 : viewmodel - getExampleListbyId 추가 (추후 update 또는 다른걸로 변경 예정)

--- lib/viewmodel/example_viewmodel.dart
+++ lib/viewmodel/example_viewmodel.dart
@@ -13,7 +13,7 @@ class ExampleViewModel extends GetxController {
   }
 
   //! 화면의 상태를 관리하기 위함.
-  Rxn<ViewState> _viewState = Rxn(Initial());
+  final Rxn<ViewState> _viewState = Rxn(Initial());
   ViewState? get viewState => _viewState.value;
 
   //!화면에서 보관할 데이터(상태)
@@ -42,6 +42,36 @@ class ExampleViewModel extends GetxController {
     }
 
     //혹시 모르니, 기존 데이터 지우고 새로운 데이터 입력
+    exampleList.clear();
+    exampleList = result as List<ExampleModel>;
+    //성공인 경우는 Loaded로 바꿔서 화면이 화면 제대로 그리도록 알려줌.
+    _viewState.value = Loaded();
+  }
+
+  Future<void> getExampleListbyId(String getId) async {
+    //서버 통신 시작 => Loading으로 화면 바꾸기
+    _viewState.value = Loading();
+
+    Either<Failure, ExampleModel> either =
+        await _repository.getExampleModelById(id: getId);
+
+    //fold를 사용하면 right, left 결과에 따라 다른 내용이 호출
+    // 아래는 left(실패)일 경우 left결과값 그대로 반환
+    //  right(성공)일 경우 right 결과값 그대로 반환
+    var result =
+        either.fold((Failure left) => left, (ExampleModel right) => right);
+
+    if (either.isLeft()) {
+      //isLeft가 true이면 실패한 상태
+      //즉, result에는 left(Failure)가 들어가 있음
+      // 그걸 담은 Error (ViewState를 상속받은)에 넣어서 셋팅
+      _viewState.value = Error(result as Failure);
+      //화면 error로 바꾸고 종료
+      return;
+    }
+
+    //혹시 모르니, 기존 데이터 지우고 새로운 데이터 입력
+    List<ExampleModel> exampleList = <ExampleModel>[];
     exampleList.clear();
     exampleList = result as List<ExampleModel>;
     //성공인 경우는 Loaded로 바꿔서 화면이 화면 제대로 그리도록 알려줌.
