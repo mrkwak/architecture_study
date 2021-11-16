6a74dd2d58debec599bc80638f4a10466d6fb2b1
mrkwak <qudcjfrhkr@naver.com>
2021-11-16 17:29:55 +0900

    추가 : 페이지 라우터 추가

--- lib/page_router.dart
+++ lib/page_router.dart
@@ -1,17 +1,23 @@
 import 'package:architecture_project/bindings/example_bindings.dart';
+import 'package:architecture_project/page/example_detail_page.dart';
 import 'package:get/get.dart';
 
 import 'page/example_page.dart';
 
 class PageRouter {
   //TODO : 자신이 개발할 페이지로 initial 네임 수정해서 개발
-  static final initial = ExamplePage.routeName;
+  static const initial = ExamplePage.routeName;
 
   static final routes = [
     //TODO: 자신의 페이지 추가 , GetXController 바인딩 필수
     GetPage(
       name: ExamplePage.routeName,
       page: () => const ExamplePage(),
+      binding: ExampleBindings(),
+    ),
+    GetPage(
+      name: ExampleDetailPage.routeName,
+      page: () => ExampleDetailPage(),
       binding: ExampleBindings(),
     ),
   ];
